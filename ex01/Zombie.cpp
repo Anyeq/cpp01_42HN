52e84d0fed9ca4cf2951b04683322cd90cb3a53c
asando <alexando17@yahoo.com>
2026-05-31 22:16:32 +0200

    WIP: ex01 all file initialisaztion

--- ex01/Zombie.cpp
+++ ex01/Zombie.cpp
@@ -5,30 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/26 22:19:23 by asando            #+#    #+#             */
-/*   Updated: 2026/05/30 23:05:13 by asando           ###   ########.fr       */
+/*   Created: 2026/05/31 22:14:28 by asando            #+#    #+#             */
+/*   Updated: 2026/05/31 22:14:29 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
-#include <iostream>
-
-Zombie::Zombie(void) {
-	std::cout << "Zombie Created" << std::endl;
-	return ;
-}
-
-Zombie::~Zombie(void) {
-	std::cout << this->_name << " is destroyed" << std::endl;
-	return ;
-}
-
-void	Zombie::setZombieName(std::string name) {
-	this->_name = name;
-	return ;
-}
-
-void	Zombie::announce(void) {
-	std::cout << this->_name << ": " << "BraiiiiiiinnnzzzZ" << std::endl;
-	return ;
-}
