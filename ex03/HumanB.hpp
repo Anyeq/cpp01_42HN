b7be98e177c16fd92214abfe8228dfea185c97d5
asando <alexando17@yahoo.com>
2026-06-04 19:47:09 +0200

    WIP: init classes

--- ex03/HumanB.hpp
+++ ex03/HumanB.hpp
@@ -6,7 +6,28 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 13:12:18 by asando            #+#    #+#             */
-/*   Updated: 2026/06/04 13:12:20 by asando           ###   ########.fr       */
+/*   Updated: 2026/06/04 19:46:04 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef HUMANB_HPP
+# define HUMANB_HPP
+
+# include "Weapon.hpp"
+# include <string>
+
+class	HumanB {
+	private:
+		std::string	_name;
+		Weapon*		_weapon;
+
+	public:
+		HumanB(std::string name);
+		~HumanB(void);
+
+		void	attack(void) const;
+		void	setWeapon(Weapon& weapon);
+};
+
+#endif
 
--- ex03/Weapon.hpp
+++ ex03/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 13:11:01 by asando            #+#    #+#             */
-/*   Updated: 2026/06/04 14:45:04 by asando           ###   ########.fr       */
+/*   Updated: 2026/06/04 19:18:19 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,13 @@
 class Weapon {
 	private:
 		std::string	_type;
+
 	public:
 		Weapon(std::string type);
 		~Weapon();
+
 		const std::string&	getType() const;
-		void		setType(std::string type);
+		void				setType(std::string type);
 };
 
 #endif
