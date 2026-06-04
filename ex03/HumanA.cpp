0a8c7b72e9d73eb90263ce4f505f1932065730c9
asando <alexando17@yahoo.com>
2026-06-04 20:29:19 +0200

    WIP: create constructor descturcor and method for HumanA.cpp HumanB.cpp

--- ex03/HumanA.cpp
+++ ex03/HumanA.cpp
@@ -6,7 +6,21 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 13:12:29 by asando            #+#    #+#             */
-/*   Updated: 2026/06/04 13:12:31 by asando           ###   ########.fr       */
+/*   Updated: 2026/06/04 20:18:44 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Weapon.hpp"
+#include "HumanA.hpp"
+#include <iostream>
+
+HumanA::HumanA(std::string name, Weapon& weapon): _name(name), _weapon(weapon)
+{}
+
+HumanA::~HumanA(void){}
+
+void	HumanA::attack(void) const {
+	std::cout << this->_name << " attacks with" 
+		<< this->_weapon.getType() << std::endl;
+	return ;
+}
--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -6,7 +6,30 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 13:12:42 by asando            #+#    #+#             */
-/*   Updated: 2026/06/04 13:12:43 by asando           ###   ########.fr       */
+/*   Updated: 2026/06/04 20:28:33 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Weapon.hpp"
+#include "HumanB.hpp"
+#include <iostream>
+
+HumanB::HumanB(std::string name): _name(name), _weapon(NULL){}
+
+HumanB::~HumanB(void){}
+
+void	HumanB::attack(void) const {
+	if (this->_weapon) {
+		std::cout << this->_name << " attacks with "
+			<< this->_weapon->getType() << std::endl;
+	}
+	else {
+		std::cout << this->_name << " has no weapon" << std::endl;
+	}
+	return ;
+}
+
+void	HumanB::setWeapon(Weapon& weapon) {
+	this->_weapon = &weapon;
+	return ;
+}
