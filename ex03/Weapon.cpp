5a0f92611c5bae00a5758b84c3eb5267f6ae4c5e
asando <alexando17@yahoo.com>
2026-06-04 14:45:25 +0200

    WIP: Weapon.cpp

--- ex03/Weapon.cpp
+++ ex03/Weapon.cpp
@@ -6,7 +6,21 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 13:11:28 by asando            #+#    #+#             */
-/*   Updated: 2026/06/04 13:11:30 by asando           ###   ########.fr       */
+/*   Updated: 2026/06/04 14:45:01 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Weapon.hpp"
+
+Weapon::Weapon(std::string type): _type(type){}
+
+Weapon::~Weapon(){}
+
+const std::string&	Weapon::getType() const {
+	return (this->_type);
+}
+
+void	Weapon::setType(std::string type) {
+	this->_type = type;
+	return ;
+}
