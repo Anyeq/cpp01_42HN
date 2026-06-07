c5b8179e5f9c72103c08d961bf3167f4dc10ae20
asando <alexando17@yahoo.com>
2026-06-07 12:51:19 +0200

    WIP: Working on Replace.cpp

--- ex04/Replace.cpp
+++ ex04/Replace.cpp
@@ -6,7 +6,7 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/06 13:55:07 by asando            #+#    #+#             */
-/*   Updated: 2026/06/06 14:13:35 by asando           ###   ########.fr       */
+/*   Updated: 2026/06/06 17:54:35 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,15 @@ bool Replace::process() {
 		return (false);
 	};
 	std::ifstream infile(_filename.c_str());
+	if (!infile) {
+		std::cerr << "Error: could not open the file" << std::endl;
+		return (false);
+	}
+	std::ofstream outfile((_filename + ".replace").c_str());
+	if (!outfile) {
+		std::cerr << "Error: could not create .replace file" << std::endl;
+		return (false);
+	}
+
 	return (true);
 }
